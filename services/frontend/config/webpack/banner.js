4c8c60dc6f918295e4d994d8a6b452a29b65e88e
Andrew E. Rhyne <andrew@jobstart.com>
2016-11-02 14:40:01 -0700

    fix

--- services/frontend/config/webpack/banner.js
+++ services/frontend/config/webpack/banner.js
@@ -1,3 +1,5 @@
+// Source map support is for NodeJS source maps
+// isomorphic-fetch is a polyfill for the Apollo Client on NodeJS
 const banner = `
   require('source-map-support').install({
     environment: 'node'
